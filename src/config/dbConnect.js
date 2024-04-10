584c15265e56d8d285733e8a7a26b1849efb5d22
Carolina Moutella <ola@carolinamoutella.com>
2024-04-09 22:15:22 -0300

    db connect

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose, { mongo } from 'mongoose'
 
 
 async function connectToDB() {
-  mongoose.connect(process.env.DB_CONNECTION_STR)
+  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@alurabooks.waigkzc.mongodb.net/livraria?retryWrites=true&w=majority&appName=AluraBooks`)
 
   return mongoose.connection
 }
